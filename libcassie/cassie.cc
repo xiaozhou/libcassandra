9d2b95a835f73fa19529c1310a30c836d93d6f65
Mina Naguib <mina.git@naguib.ca>
2010-05-04 12:47:50 -0400

    Empty libcassie shell

---
+++ libcassie/cassie.cc
@@ -0,0 +1,16 @@
+/*
+ * LibCassie
+ * Copyright (C) 2010 Mina Naguib
+ * All rights reserved.
+ *
+ * Use and distribution licensed under the BSD license. See
+ * the COPYING file in the parent directory for full text.
+ */
+#include "cassie.h"
+
+extern "C" {
+
+	void cassie_init(void) {
+	}
+
+}
---
+++ libcassie/cassie.h
@@ -0,0 +1,24 @@
+/* 
+ * LibCassie
+ * Copyright (C) 2010 Mina Naguib
+ * All rights reserved.
+ *
+ * Use and distribution licensed under the BSD license.  See
+ * the COPYING file in the parent directory for full text.
+ *
+ */
+
+#ifndef __LIBCASSIE_H
+#define __LIBCASSIE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cassie_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
