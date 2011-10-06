e386204f51027b6a863ffa5809fe3652bb706b28
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-10-06 15:28:53 -0400

    Factor potentially platform specific includes into a separate header, convert to using boosts tr1 includes to fix some cross-platform issues (specifically, failures with mac's builtin tr1 implementation).

---
+++ libcassandra/util/platform.h
@@ -0,0 +1,22 @@
+/*
+ * LibCassandra
+ * Copyright (C) 2010-2011 Padraig O'Sullivan, Ewen Cheslack-Postava
+ * All rights reserved.
+ *
+ * Use and distribution licensed under the BSD license. See
+ * the COPYING file in the parent directory for full text.
+ */
+
+#ifndef __LIBCASSANDRA_UTIL_PLATFORM_H
+#define __LIBCASSANDRA_UTIL_PLATFORM_H
+
+#include <boost/tr1/memory.hpp>
+#include <boost/tr1/tuple.hpp>
+#include <boost/tr1/unordered_map.hpp>
+
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+#endif //__LIBCASSANDRA_UTIL_PLATFORM_H
