0b7aa15019283753a620c908deea7bab4da1f94d
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-10-07 14:08:31 -0700

    Disable boost's include_next functionality, fixes compatibility with Sirikata.

--- libcassandra/util/platform.h
+++ libcassandra/util/platform.h
@@ -10,6 +10,20 @@
 #ifndef __LIBCASSANDRA_UTIL_PLATFORM_H
 #define __LIBCASSANDRA_UTIL_PLATFORM_H
 
+#ifdef __GNUC__
+
+// #include_next is broken: it does not search default include paths!
+#define BOOST_TR1_DISABLE_INCLUDE_NEXT
+// config_all.hpp reads this variable, then sets BOOST_HAS_INCLUDE_NEXT anyway
+#include <boost/tr1/detail/config_all.hpp>
+#ifdef BOOST_HAS_INCLUDE_NEXT
+// This behavior has existed since boost 1.34, unlikely to change.
+#undef BOOST_HAS_INCLUDE_NEXT
+#endif
+
+#endif
+#endif
+
 #include <boost/tr1/memory.hpp>
 #include <boost/tr1/tuple.hpp>
 #include <boost/tr1/unordered_map.hpp>
